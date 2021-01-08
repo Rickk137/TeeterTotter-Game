a81ff7f3f0a5839f8377ab1b4e6b74e9a8d0fe37
peyman <peymannaderi3@yahoo.com>
2021-01-08 16:32:09 +0330

    game component init.

---
+++ src/components/Game/TeeterTotter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    TeeterTotter
+    <div
+      class="seasaw"
+      :style="{transform: `rotate(${this.sesawAngle}deg)`}"
+    />
+    <div class="pivot" />
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    sesawAngle () {
+      return 10
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.pivot {
+  border-radius: 2px;
+  border-bottom: 120px solid gray;
+  border-left: 40px solid transparent;
+  border-right: 40px solid transparent;
+  width: 0;
+  height: 0;
+  margin: auto;
+}
+.seasaw {
+  height: 5px;
+  width: 100%;
+  background-color: red;
+  transition: 500ms ease-in;
+  position: relative;
+}
+</style>
---
+++ src/components/Game/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="game layout-v ">
+    <div style="height: 500px;background: #eaeaea;">fall</div>
+    <TeeterTotter />
+  </div>
+</template>
+
+<script>
+import TeeterTotter from "./TeeterTotter"
+export default {
+  components: {
+    TeeterTotter,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.game {
+  width: 90vw;
+  margin: auto;
+  max-width: 1000px;
+  min-width: 500px;
+}
+</style>
